427df25788b2b976fa2c0617254e9f494691aaee
bemily1 <156600946+bemily1@users.noreply.github.com>
2025-04-21 10:33:55 -0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import {useEffect, useReducer} from "react";
+import './App.css';
+import chef from "./images/chef.jpg";
+
+function Header ({ name, year }) {
+  return (
+    <header>
+      <h1>{name}'s Kitchen</h1>
+      <p>Copyright {year}</p>
+    </header>
+  );
+}
+
+const items = [
+  "Macaroni and Cheese",
+  "Salmon with Potatoes",
+  "Tofu with Vegetables",
+  "Minestrone Soup"
+];
+
+const dishObjects = items.map((dish, i) => ({
+  id: i,
+  title: dish
+}));
+
+console.log(dishObjects);
+
+function Main({ dishes, openStatus, onStatus }) {
+  return (
+    <>
+      <div>
+        <button onClick={()=> onStatus("true")}>I want to be open</button>
+        <h2>Welcome to this beautiful restaurant! {openStatus ? "Open" : "Closed"}</h2>
+      </div>
+      <main>
+        <img 
+          src={chef} 
+          height={200} 
+          alt="A photo of a smiling chef owner" 
+        />
+        <ul>
+          {dishes.map((dish) => (
+            <li key={dish.id} style={{ listStyleType: "none"}}>
+              {dish.title}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
+}
+
+// App function to render components in the App component
+function App() {
+  const [status, toggle] = useReducer(
+    (status) => !status,
+     true
+    );  // set initial status to Open
+
+    useEffect(() => {
+      console.log(`The restaurant is ${status ? "open" : "closed"}`);
+    }, [status]);  //Run on first render or everytime when status changes
+
+  return (
+    <div>
+      <h1>The restaurant is currently {" "}
+        {status ? "open" : "closed"}.
+      </h1>
+      <button onClick={toggle}>
+        {status ? "Close" : "Open"} Restaurant
+      </button>
+      <Header name="Alex" year={new Date().getFullYear()}/> 
+      <Main 
+      dishes={dishObjects}
+      openStatus={status} 
+      onStatus={toggle} 
+      />
+    </div>
+  );
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+
+createRoot(document.getElementById('root')).render(<App />);  // find the root element and render the App component
