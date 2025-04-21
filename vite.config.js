89a4a0a8a486131bb9ccd369574a5141717e06d3
bemily1 <156600946+bemily1@users.noreply.github.com>
2025-04-21 10:47:55 -0400

    Config changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/M05_Tutorial/',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js'
+      }
+    }
+  }
 })
